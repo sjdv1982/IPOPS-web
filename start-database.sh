baccf4dcf010bfeca036795503d538c8dc654017
Sjoerd de Vries <sjdv1982@gmail.com>
2021-07-21 17:16:39 +0200

    minor

--- start-database.sh
+++ start-database.sh
@@ -5,4 +5,4 @@ docker run \
   -v ~/IPOPS-db:/data \
   -u `id -u` \
   --group-add users \
-  rpbs/seamless start.sh python3 -u /home/jovyan/seamless-tools/database.py /data/database-config.yaml
+  rpbs/seamless start.sh python3 -u /home/jovyan/seamless-tools/database.py /data/config/database-config.yaml
