5c19a661066ef01078fae42bc7208269f0e13c1c
Sjoerd de Vries <sjdv1982@gmail.com>
2021-07-22 09:32:35 +0200

    included database config in Git repo

--- start-database.sh
+++ start-database.sh
@@ -1,8 +1,11 @@
+DATABASE_DIR=~/IPOPS-db
+mkdir -p $DATABASE_DIR/config
+cat database-config.yaml > $DATABASE_DIR/config/database-config.yaml
 docker run \
   -d \
   --network=host \
   --name seamless-database-container \
-  -v ~/IPOPS-db:/data \
+  -v $DATABASE_DIR:/data \
   -u `id -u` \
   --group-add users \
   rpbs/seamless start.sh python3 -u /home/jovyan/seamless-tools/database.py /data/config/database-config.yaml
